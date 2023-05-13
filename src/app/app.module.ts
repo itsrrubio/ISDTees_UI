883355dd0d5a0fd27bdf49d758d674e59bca855d
itsrrubio <rarubio1@gmail.com>
2023-05-13 13:02:50 -0700

    Adding products, footer and header components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
